758032fb634f420e67d15fab80d8dbb75f9a28bd
Michał Jaśkowiak <58137172+mjaskowiak@users.noreply.github.com>
2020-07-12 16:35:38 +0200

    Utworzenie modelu posta, wstępne utworzenie modelu komentarza, początek nadawania uprawnień administracyjnych w pliku admin.py

--- Blogowanie/admin.py
+++ Blogowanie/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from.models import Post, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'id_user', 'publish', 'status')
+    list_filter = ('publish',)
+    search_fields = ('title', 'body')
+    date_hierarchy = ('publish')
+    ordering = ('status', 'publish')
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'id_user', 'created', 'active')
+    # list_filter = ('publish',)
+    search_fields = ('id_user', 'body')
--- Blogowanie/models.py
+++ Blogowanie/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+# class User:
+
+
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft', 'Draft'),
+        ('published', 'Published')
+    )
+    # id_post =
+    # id_blog =
+    title = models.CharField(max_length=250)
+    id_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    comments = models.TextField(null=True, blank=True)
+    publish = models.DateTimeField(default=timezone.now)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='draft')
+
+    class Meta:
+        ordering = ('-publish',)
+
+    def __str__(self):
+        return self.title
+
+# class Comment(models.Model):
+#     # id_comment =
+#     # id_user =
+#     id_post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+#     body = models.TextField()
+#     created = models.DateTimeField(auto_now_add=True)
+#     updated = models.DateTimeField(auto_now=True)
+#     # active = models.BooleanField(default=True) - do zarządzania przez admina czy komentarz ma być wiudoczny dla wszystkich
+#
+#     class Meta:
+#         ordering = ('created',)
+#
+#     def __str__(self):
+#         return 'Commend add by {} for post {}'.format(self.id_user, self.id_post)
