b8bca81af22f14c7e4325a717df92471174303b0
mateusz <mkowalski234@gmail.com>
2020-07-13 18:34:53 +0200

    utworzenie plików html odpowiadającym widoką

--- Blogowanie/views.py
+++ Blogowanie/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 def Register(request):
-    return render(request, 'base.html')
+    return render(request, 'register.html')
 def Login(request):
-    return render(request, 'base.html')
+    return render(request, 'login.html')
 def User_view(request):
-    return render(request, 'base.html')
+    return render(request, 'user_view.html')
 def Admin_view(request):
-    return render(request, 'base.html')
+    return render(request, 'admin_view.html')
 def Guest_view(request):
-    return render(request, 'base.html')
+    return render(request, 'guest_view.html')
