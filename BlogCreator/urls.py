8a6f42ee2aec22301e09f09bd6480ad88bba50d8
mateusz <mkowalski234@gmail.com>
2020-07-13 18:12:05 +0200

    prymitywne views oraz url do views stworzone

--- BlogCreator/urls.py
+++ BlogCreator/urls.py
@@ -16,6 +16,13 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Blogowanie import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('/',views.Guest_view,name='main'),
+    path('login/',views.Login,name='login'),
+    path('register/',views.Register,name='register'),
+    path('/logged',views.User_view,name='main'),
+    path('administrate/',views.Admin_view,name='main') # todo pojawia się pytanie, trzeba zrobić algorytm sprawdzający kim jest użytkownik aby pokazywało odpowiedni view
 ]
--- Blogowanie/views.py
+++ Blogowanie/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.http import HttpResponse
+def Register(request):
+    return render(request, 'base.html')
+def Login(request):
+    return render(request, 'base.html')
+def User_view(request):
+    return render(request, 'base.html')
+def Admin_view(request):
+    return render(request, 'base.html')
+def Guest_view(request):
+    return render(request, 'base.html')
