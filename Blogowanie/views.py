cc89603689c22070f829e21b778bae4fc4d728fb
Michał Jaśkowiak <58137172+mjaskowiak@users.noreply.github.com>
2020-07-24 19:31:49 +0200

    praca nad formularzami

--- Blogowanie/views.py
+++ Blogowanie/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
-from .models import Post, User
+from .models import Post, User, Blog, Comment
+from django.contrib.auth.decorators import login_required
+from .forms import BlogForm, UserForm, CommentForm, PostForm
 
+# def Blogs(request):
+#     all_blogs = Blog.objects.all()
+#     return render(request, )
 
 def Register(request):
     return render(request, 'register.html')
@@ -18,11 +23,13 @@ def Main_view(request):
 def Admin_view(request):
     return render(request, 'admin_view.html')
 
+
+
 def post_list(request):
     posts = Post.published.all()
     return render(request,'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
+    post = get_object_or_404(Post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
     return render(request, 'blog/post/detail.html', {'post': post})
